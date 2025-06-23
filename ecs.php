d82329820506fb19125a696558b164b8a7739abf
Wojdylak <karolwojdyla37@gmail.com>
2025-06-23 13:15:38 +0200

    Init

---
+++ ecs.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->import('vendor/sylius-labs/coding-standard/ecs.php');
+
+    $ecsConfig->parallel();
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests/Behat',
+        __DIR__ . '/ecs.php',
+    ]);
+
+    $ecsConfig->ruleWithConfiguration(
+        HeaderCommentFixer::class,
+        [
+            'location' => 'after_open',
+            'comment_type' => HeaderCommentFixer::HEADER_COMMENT,
+            'header' => <<<TEXT
+This file is part of the Sylius package.
+
+(c) Sylius Sp. z o.o.
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+TEXT
+        ],
+    );
+};
---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\McpServerPlugin\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    /**
+     * @psalm-suppress UnusedVariable
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('acme_sylius_example');
+        $rootNode = $treeBuilder->getRootNode();
+
+        return $treeBuilder;
+    }
+}
